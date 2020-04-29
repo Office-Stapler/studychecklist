1760e1d9d0a98c5b7687e7eb7fe59898d4226f1a
Raymond Lam <z5255290@unsw.edu.au>
2020-04-29 21:05:55 +1000

    changed feature

--- data/unswscraper.py
+++ data/unswscraper.py
@@ -7,31 +7,59 @@ def get_info_subject(subject):
     url = f'http://timetable.unsw.edu.au/2020/{subject}KENS.html'
     page = requests.get(url)
     soup = bs4.BeautifulSoup(page.text, 'html.parser')
-    subjects = soup.find_all('td', class_='data')[9:]
-    names = [x.get_text() for x in subjects[:-1:3]]
-    units = []
-    for i in subjects[1:-1:3]:
-        try:
-            unit = int(i.get_text())
-        except:
-            continue
-        units.append(unit)
+    subjects = soup.find_all('td', class_='formBody')
+    ugrad = list(filter(lambda x: x != '', subjects[3].get_text().split('\n')))[3::]
+    try:
+        pgrad = list(filter(lambda x: x != '', subjects[5].get_text().split('\n')))[3::]
+    except:
+        pgrad = []
+    try:
+        research = list(filter(lambda x: x != '', subjects[7].get_text().split('\n')))[3::]
+    except:
+        research = []
 
-    codes = [x.get_text() for x in subjects[2:-1:3]]
-    final = list(zip(codes, names, units))
+    code = []
+    title = []
+    unit = []
+    
+    code.extend(ugrad[::3])
+    title.extend(ugrad[1::3])
+    unit.extend(ugrad[2::3])
+
+    if pgrad != []:
+        code.extend(pgrad[::3])
+        title.extend(pgrad[1::3])
+        unit.extend(pgrad[2::3])
+    if research != []:
+        code.extend(pgrad[::3])
+        title.extend(research[1::3])
+        unit.extend(research[2::3])
+    final = list(zip(code, title, unit))
+
     with open('subjectinfo.json', 'r+') as f:
         courses = json.load(f)
         f.seek(0)
         courses.extend(final)
         json.dump(courses, f, indent=4)
-    
-    
+
+
 
 with open('subjectarea.json', 'r') as f:
     subjects = json.load(f)
 
-for subject in subjects:
-    get_info_subject(subject[1])
-    print(f'Finished dumping {subject}...')
+for title, subject in subjects:
+    get_info_subject(subject)
+    print(f'Finished dumping {subject}....')
 
+""" for subject in subjects:
+    get_info_subject(subject[1])
+    print(f'Finished dumping {subject}...') """
+""" 
+for subject in subjects:
+
+        course = list(filter(lambda x: x != '', course))
+        code.extend(course[::3])
+        names.extend(course[1::3])
+        units.extend(course[2::3])
+    print(list(zip(code, names, units))[8::]) """
 
